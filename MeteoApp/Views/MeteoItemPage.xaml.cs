8f3fd7b156b605e13607a366e10269f8320bbe0f
Luca Giussani <lucagiussani.lg@gmail.com>
2019-04-05 13:22:22 +0100

    primo push progetto iniziale

---
+++ MeteoApp/Views/MeteoItemPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MeteoApp
+{
+    public partial class MeteoItemPage : ContentPage
+    {
+        public MeteoItemPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
